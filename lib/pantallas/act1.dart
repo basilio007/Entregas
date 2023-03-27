ef3888168fee84612a8e89f2feb94d760d570d75
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-03-27 17:16:59 +0200

    rre

--- lib/pantallas/act1.dart
+++ lib/pantallas/act1.dart
@@ -4,32 +4,29 @@ import 'package:flutter/material.dart';
 class colores extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
-
-      child:Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.yellow,
-
-          ),
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.red,
-
-          )
-
-        ],
-      ),
-    );
+   return Scaffold(
+     backgroundColor: Colors.teal,
+body: Row(
+  children: [
+    Container(
+      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      color: Colors.amber,
+      width: 100,
+    ),
 
 
 
+    Container(
+      margin: EdgeInsets.fromLTRB(758, 10, 0, 10),
+      color: Colors.red,
+      width: 100,
+    ),
+
+
+  ],
+),
+
+   );
   }
 
 }
